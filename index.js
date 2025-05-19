0a964c6e570f495f39c751abf65980c5127396a2
nayon117 <hasibul.nayon1@gmail.com>
2025-05-19 08:22:29 +0600

    setup express and mongodb

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb connnection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oesmq38.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/",(req,res)=>{
+  res.send("Hello world");
+})
+
+app.listen(port,()=>{
+  console.log('dine pro listening')
+})
