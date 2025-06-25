ddcce6adfd253b7da77b5e59f23bb39601c8e7be
nayon117 <hasibul.nayon1@gmail.com>
2025-06-25 16:53:24 +0600

    create cart collection

--- index.js
+++ index.js
@@ -23,7 +23,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    const menuColl = client.db("dinepro").collection("menu");
+    const reviewColl = client.db("dinepro").collection("reviews");
+    const cartColl = client.db("dinepro").collection("carts");
+
+    // menu coll
+    app.get("/menu",async(req,res)=>{
+      const result = await menuColl.find().toArray();
+      res.send(result); 
+    })
+    // review coll
+    app.get("/reviews",async(req,res)=>{
+      const result = await reviewColl.find().toArray();
+      res.send(result); 
+    })
+
+    // cart coll
+
+    app.get("/carts",async(req,res)=>{
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartColl.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post("/carts",async(req,res)=>{
+      const cartItem = req.body;
+      const result = await cartColl.insertOne(cartItem);
+      res.send(result);
+    })
+
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
